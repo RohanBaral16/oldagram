7a46a12a4b6718be63943f7a59794d4b2660bdef
rohanbaral16 <rhnbrl16@gmail.com>
2025-08-17 15:58:17 +0545

    basic front end rendered

--- index.js
+++ index.js
@@ -28,3 +28,39 @@ const posts = [
     }
 ]
 
+const mainContent = document.getElementById("main-container");
+// let's define a render function 
+
+function renderPost(posts){
+
+        let contentToPrint = `
+        <div class="content">
+                <div class="post-profile">
+                    <img class="user-profile-pic" src="${posts.avatar}">
+                    <div class="post-profile-details">
+                        <p class="profile-name">${posts.name}</p>
+                        <p class="profile-location">${posts.location}</p>
+                    </div>
+                </div>
+
+                <img class="post-pic" src="${posts.post}" alt="user's post">
+                <div class="interaction-buttons">
+                    <img class="interactive-icons" src="images/icon-heart.png">
+                    <img class="interactive-icons" src="images/icon-comment.png">
+                    <img class="interactive-icons" src="images/icon-dm.png">
+                </div>
+                <p class="like-count">${posts.likes}</p>
+                <p class="user-caption"><span class="username">${posts.username}</span>${posts.comment}</p>
+            </div>
+            <div class="spacing"></div>
+        `
+        mainContent.innerHTML += contentToPrint
+}
+
+function postLooper(){
+    for (let i=0; i<posts.length ; i++){
+        renderPost(posts[i])
+    }
+}
+
+postLooper()
